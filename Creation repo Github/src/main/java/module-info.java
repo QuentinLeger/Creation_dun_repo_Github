1256dabbb5c1f2289ad1ccd76487bbfd9b593966
Quentin LEGER <quentin.leger1@etu.univ-lorraine.fr>
2026-04-21 15:10:23 +0200

    Code fonctionnel local

--- Creation repo Github/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.creationrepogithub {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.creationrepogithub to javafx.fxml;
-    exports com.example.creationrepogithub;
-}
---
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.File;
+import java.nio.file.Files;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    private TextField repoNameField;
+
+    @FXML
+    private TextField descriptionField;
+
+    @FXML
+    private TextField readmeField;
+
+    @FXML
+    public void runCommand(String command, File directory) throws Exception{
+        ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c",command);
+        builder.directory(directory);
+        builder.redirectErrorStream(true);
+        Process process = builder.start();
+        process.waitFor();
+
+    }
+    @FXML
+    public void onCreateProject() {
+        try {
+            File projectFolder = new File("C:\\Users\\atomi\\OneDrive\\Bureau\\Informatique\\Perso\\Portfolio\\Creation_dun_repo_Github\\resultatTest");
+            projectFolder.mkdirs();
+            Files.writeString(new File(projectFolder,"README.md").toPath(),"#Mon projet");
+            runCommand("git init", projectFolder);
+            runCommand("git add .", projectFolder);
+            runCommand("git commit -m \"Initial Commit\"", projectFolder);
+            System.out.println("Projet crée");
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}

8b6bea69b7f44ab41dc6c7e7c3a9a32b2a7d2f2b
Quent1 <atomiccreeperboss@gmail.com>
2026-04-23 14:39:21 +0200

    Finished project working

--- demo/src/main/java/com/example/demo/GithubRepoApplication.java
+++ demo/src/main/java/com/example/demo/GithubRepoApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class GithubRepoApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Création repo github!");
+        FXMLLoader fxmlLoader = new FXMLLoader(GithubRepoApplication.class.getResource("githubRepo-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 400);
+        stage.setTitle("Repo github creation!");
         stage.setScene(scene);
         stage.show();
     }
